8db5a7af86f61040f2b5259b2eb1bed59b060585
Jaime Rangel S <138807473+jaimemu@users.noreply.github.com>
2026-08-11 15:33:40 -0500

    Add Vectores class to display array elements

---
+++ vectores j.java
@@ -0,0 +1,15 @@
+package com.mycompany.vectores;
+
+import java.util.Scanner;
+
+public class Vectores {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int x[] = {1, 3, 10, 11};
+        System.out.println("Posicion 0 contiene" + x[0]);
+        System.out.println("Posicion 1 contiene" + x[1]);
+        System.out.println("Posicion 2 contiene" + x[2]);
+        System.out.println("Posicion 3 contiene" + x[3]);
+    }
+}
